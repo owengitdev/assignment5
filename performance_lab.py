d31b2b35453550f57413892c0c71cfb10fdbf244
owengitdev <yanyida19790228@gmail.com>
2025-10-04 23:09:44 -0400

    Update performance_lab.py

--- performance_lab.py
+++ performance_lab.py
@@ -6,18 +6,26 @@
 # Input: [1, 3, 2, 3, 4, 1, 3]
 # Output: 3
 
+
 def most_frequent(numbers):
     # Your code here
-    pass
+    return max(set(numbers), key=numbers.count)
+Numbers = [1, 2, 3, 4, 5, 2, 3]
+print(most_frequent(Numbers))
 
 """
 Time and Space Analysis for problem 1:
-- Best-case:
-- Worst-case:
-- Average-case:
-- Space complexity:
+- Best-case: 2
+- Worst-case: 5 
+- Average-case: 2
+- Space complexity: 7
+
 - Why this approach?
+The reason this approach is beneficial since it can find what's the most frequent number that is shown
+on the lists. 
 - Could it be optimized?
+Yes, it can be optimized but it'll require some additional coding in order for the function to be 
+optimized.
 """
 
 
@@ -30,16 +38,26 @@ Time and Space Analysis for problem 1:
 
 def remove_duplicates(nums):
     # Your code here
-    pass
+    nums = [4, 5, 4, 6, 5, 7]
+    seen = []
+
+    for nums in Numbers:
+        if nums not in seen:
+            Numbers.append(nums)
+        print(seen)
+
 
 """
 Time and Space Analysis for problem 2:
-- Best-case:
-- Worst-case:
-- Average-case:
-- Space complexity:
+- Best-case:  4
+- Worst-case: 6
+- Average-case: 4
+- Space complexity: 7
 - Why this approach?
+So this approach on remove some duplicates that is shown in the list
+and can shorten the list until there are no duplicates.
 - Could it be optimized?
+Yes, It's possible. 
 """
 
 
@@ -53,7 +71,13 @@ Time and Space Analysis for problem 2:
 
 def find_pairs(nums, target):
     # Your code here
-    pass
+    result = ([1, 2, 3, 4])
+    for item in nums:
+        if item == target:
+            result.append(item * 2)
+        return result
+    return None
+
 
 """
 Time and Space Analysis for problem 3:
@@ -78,11 +102,15 @@ def add_n_items(n):
     # Your code here
     pass
 
+
 """
 Time and Space Analysis for problem 4:
 - When do resizes happen?
+Resize can happen if the list is too much.
 - What is the worst-case for a single append?
+
 - What is the amortized time per append overall?
+
 - Space complexity:
 - Why does doubling reduce the cost overall?
 """
@@ -97,16 +125,25 @@ Time and Space Analysis for problem 4:
 # Output: [1, 3, 6, 10]
 # Because: [1, 1+2, 1+2+3, 1+2+3+4]
 
-def running_total(nums):
+def running_total():
     # Your code here
-    pass
+    nums = [1, 2, 3, 4]
+    return nums
+
+
+res = running_total()
+print(res)
 
 """
 Time and Space Analysis for problem 5:
-- Best-case:
-- Worst-case:
-- Average-case:
-- Space complexity:
+- Best-case: 2
+- Worst-case: 3
+- Average-case: 4
+- Space complexity: 1
+
 - Why this approach?
+This approach is beneficial since it can sum up some numbers in the list and can print out
+the full total of the number that is added together.
 - Could it be optimized?
+Yes, it can be optimized.
 """
